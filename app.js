753a89cf77c74138c8a5d671f430fb9217df1cc9
Kati Lue <katillueth@gmail.com>
2016-02-15 17:28:36 -0600

    completed project

--- app.js
+++ app.js
@@ -1,43 +1,25 @@
 var counter = 0;
 $(document).ready(function() {
 
-	$('#generate').append('<button class="generate-button">Generate</button>');
-
-	$('.generate-button').on('click', appendDOM )
-	$('#generate').on('click', '.clickNumber', showCount);
-
-
-
-$('#delete').on('click', function() {
-		$('#delete').remove();
-	});
-
-$('#change').on('click', function() {
-
-		$('#container').css("background-color", "white");
-		$(this).css("background-color", "red");
-	});
-
-
-
-
-
+	$('.buttonDiv').on('click', '.generateButton', appendDOM);
+	$('.container').on('click', '.delete-button', deleteButton);
+	$('.container').on('click', '.change-button', changeColor);
 });
-
 
 function appendDOM() {
 	counter++;
-	$('#container').append('<button class="delete-button" id="delete">Delete</button>');
-	$('#container').append('<button class="change-button" id="change">Change</button>');
+	var newDiv = $('.container').append('<div class="newDiv"></div>');
 
-	var $el = $('#generate').children().last();
-	$el.data("clickValue", counter);
-	
-	$el.append('<p class="clickNumber">Clicked: ' + counter + '</p>');
+	$('.container').children().last().append('<button class="delete-button">Delete</button>');
+	$('.container').children().last().append('<button class="change-button">Change</button>');
+
+	var $el = $('#countNumber').text('Clicked: '+ counter);
 }
 
-function showCount(){
-	console.log($(this).parent().data(''))
+function deleteButton(){
+		$(this).parent().remove();
 }
 
-
+function changeColor(){
+	$(this).parent().toggleClass("colored");
+}
